0c76853e53f27b4c3a8b2a490fb9e270ac966f61
Surya <surya@whitehatengineering.com>
2017-12-27 10:39:13 +0545

    Implement function remove and insert

--- List/index.js
+++ List/index.js
@@ -1,4 +1,6 @@
 const MINIMUM_SIZE = 0
+const TRUE = true
+const FALSE = false
 
 class List {
 
@@ -14,12 +16,37 @@ class List {
   }
   // Postcondition: Add parameter element in next position of data store
 
+  // Preconditions: elements are expected to be single data type.
   find(element){
+    // Iterates loop linearly over all elements in array and return matching element index
     for (let index = 0; index < this.listSize; ++index){
       return this.dataStore[index] === element && index
     }
     return -1
   }
+  // Postcondition: returns index of an element if matches else return -1
 
+  // Returns size of an list
+  length(){
+    return this.listSize;
+  }
 
+  remove(element){
+    element_index = this.find(element)
+    if (element_index > -1){
+      this.dataStore.splice(element_index, element)
+      --this.listSize
+      return TRUE
+    }else{
+      return FALSE
+    }
+  }
+
+  toString(){
+    return this.dataStore;
+  }
+
+  insert(element, after){
+      
+  }
 }
